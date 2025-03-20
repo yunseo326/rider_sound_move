53d7fd4872ccefeee73fbbda75e30d220a7d641d
hwangyunseo326 <hwangyunseo326@gmail.com>
2025-03-20 11:56:16 +0900

    오디오 파일 실행 및 반환 코드 추가

--- robot_connection-master/src/my_test_pkg_py/my_test_pkg_py/control.py
+++ robot_connection-master/src/my_test_pkg_py/my_test_pkg_py/control.py
@@ -1,28 +1,35 @@
+import subprocess 
+import rclpy, time 
+from rclpy.node import Node 
+from std_msgs.msg import UInt32 
+from geometry_msgs.msg import Vector3, Twist 
+from nav_msgs.msg import Odometry 
+from std_srvs.srv import Empty 
+
+EXE_PATH = "/home/muwon/sound_and_test/Sound-source-localization-using-TDOA-main/final/build/main"
+
+
+def Audio():
+
+    result = subprocess.run(EXE_PATH, capture_output=True, text=True)
+    print(result.stdout)
+    return result 
+
 """
 Example to move the robot in blind mode using ROS2 API without services.
 """
-import rclpy, time
-from rclpy.node import Node
-from std_msgs.msg import UInt32
-from geometry_msgs.msg import Vector3, Twist
-from nav_msgs.msg import Odometry  # 메시지 타입 변경 가능
-from std_srvs.srv import Empty
-from sensor_msgs.msg import LaserScan  # 메시지 타입 변경 가능
-import numpy as np
-#import reinforcement_model
-import torch
 
 Iteration = 100
 Speed_X = 0.5
 Speed_Y = 0.5
-Speed_Angular = 0.5
+Speed_Angular = 1.0
 
 class TestMoveBlindNoService(Node):
     def __init__(self):
         super().__init__("MoveBlindNoService")
         self.pub_action = self.create_publisher(UInt32, '/command/setAction', 10)
+        self.pub_run = self.create_publisher(UInt32, '/command/setRun', 10)
         self.pub_control_mode = self.create_publisher(UInt32, '/command/setControlMode', 10)
-
         self.pub_twist = self.create_publisher(Twist, '/mcu/command/manual_twist', 10)
         self.minimal_subscriber = MinimalSubscriber()
 
@@ -73,11 +80,35 @@ class TestMoveBlindNoService(Node):
         self.get_logger().info("Setting action=0")
         self.pub_twist.publish(Twist()) # zero twist
 
+    def TurnRight(self, angular):
+        self.get_logger().info("Commanding TurnRight twist")
+        rclpy.spin_once(self.minimal_subscriber)
+        initial_angular = self.minimal_subscriber.angular
+        changed_angular = angular
+        while abs(changed_angular - initial_angular - angular) >= 0.05:
+            rclpy.spin_once(self.minimal_subscriber)
+            changed_angular = self.minimal_subscriber.angular
+            self.pub_twist.publish(Twist(angular=Vector3(z=(changed_angular-initial_angular-angular))))
+            time.sleep(0.01) # do this instead of sleep(2) to avoid timeout
+            print((changed_angular-initial_angular) * 10)
+
+        self.get_logger().info("Setting action=0")
+        self.pub_twist.publish(Twist()) # zero twist
+
+    def TurnLeft(self, angular):
+        self.get_logger().info("Commanding TurnLeft twist")
+        for i in range(angular):
+            self.pub_twist.publish(Twist(angular=Vector3(z=-Speed_Angular)))
+            time.sleep(0.01) # do this instead of sleep(2) to avoid timeout
+
+        self.get_logger().info("Setting action=0")
+        self.pub_twist.publish(Twist()) # zero twist
+
     def Endmode(self):
-        node.pub_action.publish(UInt32(data=0)) # sit
+        self.pub_action.publish(UInt32(data=0)) # sit
         time.sleep(5)
-        node.get_logger().info("Setting control mode=180")
-        node.pub_control_mode.publish(UInt32(data=180))
+        self.get_logger().info("Setting control mode=180")
+        self.pub_control_mode.publish(UInt32(data=180))
 
 
 class MinimalSubscriber(Node):
@@ -85,8 +116,8 @@ class MinimalSubscriber(Node):
         super().__init__('minimal_subscriber')
         self.subscription = self.create_subscription(
             Odometry,  # 수신할 메시지 타입
-            '/odom',  # 구독할 토픽 이름
-            self.odom_callback,  # 콜백 함수
+            '/odom',    # 구독할 토픽 이름
+            self.odom_callback,   # 콜백 함수
             10  # 큐 크기
         )
         self.subscription  # 방출 방지
@@ -110,102 +141,39 @@ class MinimalSubscriber(Node):
         angular_z = msg.twist.twist.angular.z
 
         # 로그 출력
-        self.get_logger().info(f"위치: x={x:.2f}, y={y:.2f}, z={z:.2f}")
+        self.get_logger().info(f"위치 : x={x:.2f}, y={y:.2f}, z={z:.2f}")
         self.get_logger().info(f"자세(쿼터니언): qx={qx:.2f}, qy={qy:.2f}, qz={qz:.2f}, qw={qw:.2f}")
         self.get_logger().info(f"속도: 선속도 x={linear_x:.2f}, 각속도 z={angular_z:.2f}")
 
-# 라이브러리 불러오기
-import torch
-import torch.nn.functional as F
-
-state_size = 93
-action_size = 5
-
-class ActorCritic(torch.nn.Module):
-    def __init__(self, **kwargs):
-        super(ActorCritic, self).__init__(**kwargs)
-        self.d1 = torch.nn.Linear(state_size, 128)
-        self.d2 = torch.nn.Linear(128, 128)
-        self.pi = torch.nn.Linear(128, action_size)
-        self.v = torch.nn.Linear(128, 1)
-        
-    def forward(self, x):
-        x = F.relu(self.d1(x))
-        x = F.relu(self.d2(x))
-        return F.softmax(self.pi(x), dim=-1), self.v(x)
-    
-
-class LidarScan(Node):
-    def __init__(self):
-        super().__init__('sub_lidar')
-        self.subscription = self.create_subscription(Odometry,  '/odom',  self.Odom,  10)
-        self.sub_order = self.create_subscription(LaserScan, '/filtered_scan', self.Lidar, 10)
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = ActorCritic().to(self.device)
-        # 이 파일 위치를 본인에 맞게 수정해야합니다
-        self.model.load_state_dict(torch.load('/home/muwon/sound_and_test/robot_connection-master/src/my_test_pkg_py/my_test_pkg_py/pt/4action_4_model.pt'))
-        self.model.eval()
-        self.Odom_msg = np.array([0,0,0])
-
-        self.output = 0
-
-    def Lidar(self, msg):
-        self.lidar_msg = msg
-        # print(self.lidar_msg.ranges)
-        state = np.concatenate((self.lidar_msg.ranges, self.Odom_msg))
-        state[state == np.inf] = 15
-        state = np.float32(state)
-        self.output, _ = self.model(torch.FloatTensor(state).to(self.device))
-        self.action = torch.multinomial(self.output,1).cpu().numpy()[0]
-
-        print(self.action)
-
-
-        # print(state) 
-        # print(len(state))
-        # print("\n")
-
-    def Odom(self, msg):
-
-        position = [msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z]
-        self.Odom_msg = np.array(position)
-
+        self.get_logger().info(msg.pose.pose)
 
 def main():
     rclpy.init(args=None)
     node = TestMoveBlindNoService()
     minimal_subscriber = MinimalSubscriber()
-    lidar = LidarScan()
 
     node.Initialize()
-    print("forward : 1 \n backward : 2 \n right : 3 \n left : 4 \n end : 7 \n ")
-    
-    lidar.action = 0
     while True:
-        output = rclpy.spin_once(lidar)
-        if lidar.action == 1:
-            print("forward")
-            node.Forward()
-            rclpy.spin_once(minimal_subscriber)
-        elif lidar.action == 2:
-            print("behind")
-            node.Backward()
-            rclpy.spin_once(minimal_subscriber)
-        elif lidar.action == 3:
-            print("right")
-            node.RightSide()
-            rclpy.spin_once(minimal_subscriber)
-        elif lidar.action == 4:
-            print("left")
-            node.LeftSide()
-            rclpy.spin_once(minimal_subscriber)
-        
-        elif lidar.action == 5:
-            node.Endmode()
-            break
+        degree = Audio()
+
+        if  0 < degree < 180 :
+            step = degree // 30
+            while step >=0:
+                step = step - 1
+                node.TurnLeft()
+
+        elif degree < 360 :
+            step = (360 - degree) // 30
+            while step >=0:
+                step = step - 1
+                node.TurnRight()
+
+        else :
+            print("error")
 
     node.destroy_node()
     rclpy.shutdown()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__' :
     main()
