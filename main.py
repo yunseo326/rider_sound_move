2dae600eac3a6eb44fb902f374cecc4e7d9a2bf2
hwangyunseo326 <hwangyunseo326@gmail.com>
2025-03-21 14:58:35 +0900

    3/21 최종 수정

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from Sound.final.control_degree import ControlDegree  # 또는 필요한 함수/클래스
-from robot.src.my_test_pkg_py.my_test_pkg_py.control import Control  # 또는 필요한 함수/클래스
+from Sound.final.control_degree import *  # 또는 필요한 함수/클래스
+from robot.src.my_test_pkg_py.my_test_pkg_py.control import *  # 또는 필요한 함수/클래스
 
 
 def main():
@@ -7,7 +7,13 @@ def main():
     # robot.src.my_test_pkg_py.my_test_pkg_py 모듈의 함수/클래스 사용
     initial = 0
     initial = ControlDegree()
-
+    control = CONTROL()
     while(initial) :
-        Control()
+        control.Control()
+        if control.lidar.action == 0:
+            initial = initial +1
+            if initial == 10:
+                break
     
+if __name__ == '__main__':
+    main()
